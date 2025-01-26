a5a8391b7c6a3e832925149e11cd75784d8f7ff7
KOUPIL Jan <koupja@delta-skola.cz>
2025-01-26 17:35:12 +0100

    Salesmen tree file deserialized

--- 04-OOP-pretezovani-kolekce/04-OOP3-040-Obchodnici/04-OOP3-040-Obchodnici/Salesman.cs
+++ 04-OOP-pretezovani-kolekce/04-OOP3-040-Obchodnici/04-OOP3-040-Obchodnici/Salesman.cs
@@ -1,19 +1,34 @@
 using System;
 using System.Collections.Generic;
+using System.Text.Json;
 
 class Salesman
 {
+    static int NextId = 1;
+
+    private int ID { get; set; }
     public string Name { get; private set; }
     public string Surname { get; private set; }
     public int Sales { get; private set; }
     public List<Salesman> Subordinates { get; private set; }
 
-    public Salesman(string surname, string name, int sales)
+    public Salesman(string surname, string name, int sales, int id = 0)
     {
         Name = name;
         Surname = surname;
         Sales = sales;
         Subordinates = new List<Salesman>();
+
+        if (id != 0)
+        {
+            ID = id;
+            if (id > NextId)
+                NextId = id + 1;
+        }
+        else
+        {
+            ID = NextId++;
+        }
     }
 
     public void AddSubordinate(Salesman subordinate)
@@ -21,4 +36,48 @@ class Salesman
         Subordinates.Add(subordinate);
     }
 
+    //kód níže slouží jen pro naèítání ze souboru
+
+    public static Salesman DeserializeTree(string jsonString)
+    {
+        List<SalesmanData> deserializedData = JsonSerializer.Deserialize<List<SalesmanData>>(jsonString);
+
+        Dictionary<int, Salesman> treeData = new Dictionary<int, Salesman>();
+        Salesman root = null;
+
+        foreach (var item in deserializedData)
+        {
+            Salesman salesman = item.ToSalesman();
+            treeData[salesman.ID] = salesman;
+
+            if (item.ParentId != 0)
+                treeData[item.ParentId].AddSubordinate(salesman);
+            else
+                root = salesman;
+        }
+
+        return root;
+    }
+
+    private class SalesmanData
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Sales { get; set; }
+        public int ParentId { get; set; }
+
+        public SalesmanData() { }
+
+        public SalesmanData(Salesman salesman, int parentId = 0)
+        {
+            ID = salesman.ID;
+            Name = salesman.Name;
+            Surname = salesman.Surname;
+            Sales = salesman.Sales;
+            ParentId = parentId;
+        }
+
+        public Salesman ToSalesman() => new Salesman(Surname, Name, Sales, ID);
+    }
 }
