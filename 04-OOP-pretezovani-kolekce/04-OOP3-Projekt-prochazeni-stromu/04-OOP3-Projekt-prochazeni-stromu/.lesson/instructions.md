46229b3145cc7c6f1afec455d271a6f8549a963e
KOUPIL Jan <koupja@delta-skola.cz>
2025-02-17 10:30:15 +0100

    Zadání projektu

--- 04-OOP-pretezovani-kolekce/04-OOP3-Projekt-prochazeni-stromu/04-OOP3-Projekt-prochazeni-stromu/.lesson/instructions.md
+++ 04-OOP-pretezovani-kolekce/04-OOP3-Projekt-prochazeni-stromu/04-OOP3-Projekt-prochazeni-stromu/.lesson/instructions.md
@@ -1,11 +1,11 @@
 # Prohlížeč obchodní sítě
 
-V zadání máte strukturu obchodní sítě z předchozího úkolu. Je u obchodníků přibyla číselná položka ID, která by se vám mohla hodit. Třídu `Salesman` nesmíte měnit
+V zadání máte strukturu obchodní sítě z předchozího úkolu. Existující kód třídy `Salesman` nesmíte měnit, můžete ale přidat další metody.
 
-Vytvoříte prohlížeč tohoto stromu a možnost vybírat si z ní a ukládat do textového souboru.
+Vytvořte prohlížeč tohoto stromu a možnost vybírat si z ní a ukládat do textového souboru.
 
 ## 1. Interaktivní prohlížeč
-* Umožněte uživateli procháze strom interaktivně. Tedy na začátku zobrazte objekt "Šéf",jeho údaje, a také jeho podřízené.
+* Umožněte uživateli procházet strom interaktivně. Tedy na začátku zobrazte objekt "Šéf",jeho údaje, a také jeho podřízené.
 * Kurzorovými klávesami a mezerníkem bude možné přejít na podřízeného nebo nadřízeného.
 ![Obchodnik](assets/obchodnik-2.gif)
 ![Obchodnik](assets/obchodnik-3.gif)
@@ -15,7 +15,7 @@ Vytvoříte prohlížeč tohoto stromu a možnost vybírat si z ní a ukládat d
 
 ## 2. Seznam 
 *  Umožněte uživateli přidat konkrétního obchodníka na seznam nebo jej z něj smazat.
-*  Seznamy ukládejte jako textové soubory v adresáři s aplikací, jejich struktura je na vás
+*  Seznamy ukládejte jako textové soubory v adresáři s aplikací, jejich struktura je na vás - nebo využijte výchozí formát
 *  Zobrazení v režimu "seznam" bude nabízet
   *  Založení nového souboru se seznamem, načtení aktuálního a uložení aktuálního seznamu (v menu)
   *  Možnost přejít na šéfa celého stromu (v menu)
@@ -32,4 +32,5 @@ Přidejte do obou menu možnost program ukončit (případně i vhodnou kláveso
 
 ## Pokyny ke zpracování
 * Problém řešte s použitím tříd a objektů, dělte řešení na rozumné celky - metody
-* Do souboru `dokumentace.md` přidejte text s popisem "jak vaše řešení funguje" - jaký je formát dat, jaké jsou v řešení třídy, co znamenají jejich vlastnosti a metody
+  * Hodně může pomoci chápat "vybratelné" bloky jako tlačítka a jejich skupiny, abstraktní objekty a jejich konkrétní implementace, mít na nich připravenu nějakou akci (Click/Select), kterou polymorfně každé tlačítko přijme/naplní apod. 
+* Do souboru `dokumentace.md` přidejte váš první pokud o dokumentaci - Plnohodnotný text s popisem "jak vaše řešení funguje" - jaký je formát dat, jaké jsou v řešení třídy, co znamenají jejich vlastnosti a metody
--- 04-OOP-pretezovani-kolekce/04-OOP3-Projekt-prochazeni-stromu/04-OOP3-Projekt-prochazeni-stromu/Salesman.cs
+++ 04-OOP-pretezovani-kolekce/04-OOP3-Projekt-prochazeni-stromu/04-OOP3-Projekt-prochazeni-stromu/Salesman.cs
@@ -1,25 +1,34 @@
 using System;
 using System.Collections.Generic;
+using System.Text.Json;
 
 class Salesman
 {
-    public int ID { get; init; }
+    static int NextId = 1;
+
+    private int ID { get; set; }
     public string Name { get; private set; }
     public string Surname { get; private set; }
     public int Sales { get; private set; }
     public List<Salesman> Subordinates { get; private set; }
 
-    private static int lastID = 0;
-
-    public Salesman(string surname, string name, int sales)
+    public Salesman(string surname, string name, int sales, int id = 0)
     {
-        ID = lastID;
-        lastID++;
-
         Name = name;
         Surname = surname;
         Sales = sales;
         Subordinates = new List<Salesman>();
+
+        if (id != 0)
+        {
+            ID = id;
+            if (id > NextId)
+                NextId = id + 1;
+        }
+        else
+        {
+            ID = NextId++;
+        }
     }
 
     public void AddSubordinate(Salesman subordinate)
@@ -27,4 +36,48 @@ class Salesman
         Subordinates.Add(subordinate);
     }
 
+    //kód níže slouží jen pro naèítání ze souboru
+
+    public static Salesman DeserializeTree(string jsonString)
+    {
+        List<SalesmanData> deserializedData = JsonSerializer.Deserialize<List<SalesmanData>>(jsonString);
+
+        Dictionary<int, Salesman> treeData = new Dictionary<int, Salesman>();
+        Salesman root = null;
+
+        foreach (var item in deserializedData)
+        {
+            Salesman salesman = item.ToSalesman();
+            treeData[salesman.ID] = salesman;
+
+            if (item.ParentId != 0)
+                treeData[item.ParentId].AddSubordinate(salesman);
+            else
+                root = salesman;
+        }
+
+        return root;
+    }
+
+    private class SalesmanData
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Sales { get; set; }
+        public int ParentId { get; set; }
+
+        public SalesmanData() { }
+
+        public SalesmanData(Salesman salesman, int parentId = 0)
+        {
+            ID = salesman.ID;
+            Name = salesman.Name;
+            Surname = salesman.Surname;
+            Sales = salesman.Sales;
+            ParentId = parentId;
+        }
+
+        public Salesman ToSalesman() => new Salesman(Surname, Name, Sales, ID);
+    }
 }
